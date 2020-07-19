70fe862f683b0f57484a0ae1c94303e3e73ce1ef
elina-wang <siaosyuan0611@gmail.com>
2020-07-19 15:44:31 +0800

    練習

---
+++ main.js
@@ -0,0 +1,84 @@
+console.log("我是JS")
+
+
+// 定義渲染區域
+// 變數 vue1 = new Vue();   -要新增一個Vue元素
+// el 指定元素 "選取器"
+
+var vue1 = new Vue ({
+    el: "#vue1",
+    data: {
+        text:"我是Vue～",
+        a:999,    //數字
+        b:2,
+        c:"2",    //字串
+    }
+});
+
+var vue2 = new Vue ({
+    el: "#vue2",
+    data: {
+        alignment:"center",
+    }
+});
+
+
+var vue3 = new Vue ({
+    el: "#vue3",
+    data: {
+        text:false,
+        activity:false,
+    }
+});
+
+var vue4 = new Vue ({
+    el: "#vue4",
+    data: {
+        products: [
+            {name:"奶茶",price:45},
+            {name:"紅茶",price:30},
+            {name:"綠茶",price:35},
+            {name:"咖啡",price:55},
+        ]
+    }
+    
+});
+
+var vue5 = new Vue ({
+    el: "#vue5",
+    data: {
+        text:"",
+        count:0,
+        t1:"",
+    },
+
+    methods:{
+        show: function () {
+            this.text = "點到了"
+        },
+
+        add: function () {
+            this.count++
+        },
+
+        sub: function () {
+            this.count--
+        },
+
+        write: function () {
+            this.t1 = "點到囉"
+        }
+
+    }
+
+
+});
+
+
+var vue6 = new Vue ({
+    el: "#vue6",
+    data: {
+        name:""
+    }
+    
+});
